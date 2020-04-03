8bc5bdedae9f743489a8a6d7e974ae7903b7d75b
JaQuan Taylor <jkaet@protonmail.com>
2020-04-03 15:16:47 -0400

    testing soemthing i know wont work

--- js/main.js
+++ js/main.js
@@ -439,3 +439,34 @@ $(function() {
   }
 
 });
+
+
+(function() {
+  var elements;
+  var windowHeight;
+
+  function init() {
+		var element = document.getElementById("myskilllist");
+  	element.classList.add(".hidden");
+    elements = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add('fade-in-element');
+        element.classList.remove('hidden');
+      }
+    }
+  }
+
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
+})();
